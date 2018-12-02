c6d5f59c00d5d351426f4c2857d2e8b2eda00619
Leonie Kallabis <leonie.kallabis@th-koeln.de>
2018-12-02 15:29:35 +0100

    initial commit

---
+++ src/main/java/de/th/koeln/fae/microservice_kalendar/MicroserviceKalendarApplication.java
@@ -0,0 +1,12 @@
+package de.th.koeln.fae.microservice_kalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceKalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceKalendarApplication.class, args);
+    }
+}
